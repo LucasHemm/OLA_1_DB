151b67ed4a60cf0697577a2bce00be80994063a4
Matias Rottbøll <mrottboell@gmail.com>
2025-02-26 10:59:25 +0100

    more setup

---
+++ OLA_1_DB/OLA_1_DB/Migrations/20250226095014_RegisterPlayerSP.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OLA_1_DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegisterPlayerSP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE registerPlayer
+            @username VARCHAR(255),
+            @email VARCHAR(255),
+            @ranking INT = NULL
+            AS
+                BEGIN
+            SET NOCOUNT ON;
+            INSERT INTO Players (username, email, ranking, created_at)
+            VALUES (@username, @email, @ranking, GETDATE());
+            END;
+                ");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql(@"DROP PROCEDURE registerPlayer");
+        }
+    }
+}
---
+++ OLA_1_DB/OLA_1_DB/Migrations/20250226095047_JoinTournamentSP.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OLA_1_DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class JoinTournamentSP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE joinTournament
+            @tournament_id INT,
+                @player_id INT
+                AS
+            BEGIN
+                SET NOCOUNT ON;
+            INSERT INTO TournamentRegistrations (tournament_id, player_id, registered_at)
+            VALUES (@tournament_id, @player_id, GETDATE());
+            END;
+                    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
